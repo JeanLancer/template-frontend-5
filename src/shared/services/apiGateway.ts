e1482a49bf05e0bb13465ec7f4acc7917ef42e1a
JeanLancerti <jeanlancerti@gmail.com>
2021-07-26 17:59:01 -0300

    feat: store

--- src/shared/services/apiGateway.ts
+++ src/shared/services/apiGateway.ts
@@ -2,16 +2,16 @@ import axios from 'axios';
 import config from '../config/index';
 
 const apiGateway = axios.create({
-  // baseURL: 'https://ecommerce.eflorista.com.br/api/v1',
-  baseURL: 'http://localhost:5555/api/v1',
+  baseURL: 'https://ecommerce.eflorista.com.br/api/v1',
+  // baseURL: 'http://localhost:5555/api/v1',
   headers: {
     credentials: `STORE_${config.KEY}`
   }
 });
 
 export const apiEflorista = axios.create({
-  // baseURL: 'https://app.eflorista.com.br/api/v1',
-  baseURL: 'http://localhost:3333/api/v1',
+  baseURL: 'https://app.eflorista.com.br/api/v1',
+  // baseURL: 'http://localhost:3333/api/v1',
   headers: {
     store_id: config.KEY
   }

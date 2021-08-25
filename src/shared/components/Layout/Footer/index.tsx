a3f1a78f73c67fb0933316e1e54df563b70b3601
JeanLancerti <jeanlancerti@gmail.com>
2021-08-25 13:58:35 -0300

    chore: add eflorista footer

--- src/shared/components/Layout/Footer/index.tsx
+++ src/shared/components/Layout/Footer/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 
 import Image from 'next/image';
 import Link from 'next/link';
